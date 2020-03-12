9a04cd931098ea621e66aa64ac1fe5b1eab3a15b
Cory Gilliam <imaaxa@gmail.com>
2020-03-12 19:33:58 -0400

    Last of the task centric updates. Switching to user centric

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,19 +1,19 @@
-import {Routes} from '@angular/router';
-import {BaseLayoutComponent} from './shared/base-layout/base-layout.component';
-import {HomeComponent} from './pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-export const AppRoutes: Routes = [
-  {
-    path: '',
-    component: BaseLayoutComponent,
-    children: [
-      {
-        path: '',
-        component: HomeComponent
-      }
-      /*
-        New components go here...
-       */
-    ]
-  }
+import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
+import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
+
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: BaseLayoutComponent },
+  { path: '', component: HomeComponent },
+  { path: 'login', component: AuthLayoutComponent }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
