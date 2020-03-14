7fcc52d0a1a52bccb090b60a9468534fecce7637
Cory Gilliam <imaaxa@gmail.com>
2020-03-13 20:10:33 -0400

    Started working on the lognin page

--- src/app/shared/auth-layout/auth-layout.component.ts
+++ src/app/shared/auth-layout/auth-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-layout',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-layout.component.css']
 })
 export class AuthLayoutComponent implements OnInit {
+  loginForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      'employeeId': new FormControl(null, Validators.required)
+    });
   }
 
+  onSubmit() {
+    console.log(this.loginForm);
+  }
 }
