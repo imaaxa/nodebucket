3bb6b8420bdff74e8e884d9bcaf4a529c1a3cf26
Cory Gilliam <imaaxa@gmail.com>
2020-03-14 17:23:04 -0400

    Added code attribution

--- src/app/shared/auth-layout/auth-layout.component.ts
+++ src/app/shared/auth-layout/auth-layout.component.ts
@@ -1,3 +1,11 @@
+/*============================================;
+Title: Node Bucket
+Author: Cory Gilliam
+Date:  March 2020;
+Modified By:
+Description: Web 450 Capstone Project.
+===========================================*/
+
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
