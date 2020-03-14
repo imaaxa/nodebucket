b32971f0bc60dd2306ab7cb9f3f49b25d609fd28
Cory Gilliam <imaaxa@gmail.com>
2020-03-13 20:10:11 -0400

    Added routes to the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
 import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
 import { HomeComponent } from './pages/home/home.component';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { AboutComponent } from './pages/about/about.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     BaseLayoutComponent,
     AuthLayoutComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +32,14 @@ import {MatIconModule} from '@angular/material/icon';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(AppRoutes, { useHash: true, enableTracing: false, scrollPositionRestoration: 'enabled'}),
+    RouterModule.forRoot(
+      AppRoutes,
+      {
+        useHash: true,
+        enableTracing: false,
+        scrollPositionRestoration: 'enabled'
+      }
+    ),
     FlexLayoutModule,
     MatToolbarModule,
     MatButtonModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,19 +1,19 @@
-import {Routes} from '@angular/router';
-import {BaseLayoutComponent} from './shared/base-layout/base-layout.component';
-import {HomeComponent} from './pages/home/home.component';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
+
+import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
 
 export const AppRoutes: Routes = [
   {
-    path: '',
-    component: BaseLayoutComponent,
+    path: '', component: BaseLayoutComponent,
     children: [
-      {
-        path: '',
-        component: HomeComponent
-      }
-      /*
-        New components go here...
-       */
+      { path: 'home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'tasks', component: TasksComponent },
+      { path: 'login', component: AuthLayoutComponent },
     ]
   }
 ];
