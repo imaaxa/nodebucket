efd933e035af524e632ce14fef624ea0667e78d0
Cory Gilliam <imaaxa@gmail.com>
2020-03-21 03:16:42 -0400

    Styling the Task component page.

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -6,7 +6,9 @@ Modified By:
 Description: Web 450 Capstone Project.
 ===========================================*/
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
+import { EditTasksComponent } from "../../edit-tasks/edit-tasks.component";
 
 @Component({
   selector: 'app-tasks',
@@ -15,9 +17,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
+  todoTasks = [
+    { _id: "5e70fc6050826e2465ba3314", text: "Second created task." },
+    { _id: "5e720eb4b63cf645968a82ea", text: "First created task." }
+  ];
+  doneTasks = [];
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  showDialog(): void {
+    console.log('Dialog open event.');
+
   }
 
 }
