e3c57ad419b507095fca9b683c0d46618cd51ac5
Cory Gilliam <imaaxa@gmail.com>
2020-03-24 07:07:42 -0400

    CORS method call fixes

--- server/app.js
+++ server/app.js
@@ -27,15 +27,16 @@ let app = express();
 
 // Set header parameters (C.O.R.S. error handling)
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  if (req.method === 'OPTIONS') {
-    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-    return res.status(200).json({});
-  }
+  res.header("Content-Security-Policy", "img-src 'self'");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
   next();
 });
 
